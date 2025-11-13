1b42d114ec756e2924b4b85a1b73440d43d4bd7b
migrater <104268939+hyoseon1201@users.noreply.github.com>
2025-11-13 21:11:59 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    
+    vector<long long> dp(n + 1, 0);
+    dp[1] = 1;
+    if (n >= 2)
+    {
+        dp[2] = 2;
+    }
+    
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+    
+    answer = dp[n];
+    
+    return answer;
+}
