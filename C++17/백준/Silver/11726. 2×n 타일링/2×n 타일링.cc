aa0bdbef3e22c1db28ad71e7cee2bdfbe8eb02c5
migrater <104268939+hyoseon1201@users.noreply.github.com>
2026-01-30 15:04:02 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int arr[1001];
+
+    cin >> n;
+
+    arr[1] = 1;
+    arr[2] = 2;
+
+    for (int i = 3; i <= 1000; i++)
+    {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+    }
+
+    cout << arr[n] << "\n";
+
+    return 0;
+}
