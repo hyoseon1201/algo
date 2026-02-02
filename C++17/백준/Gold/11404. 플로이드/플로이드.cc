6b737b678275b6e5637aaeb7f796024faa7040a8
migrater <104268939+hyoseon1201@users.noreply.github.com>
2026-02-02 17:44:15 +0900

    [Gold IV] Title: 플로이드, Time: 16 ms, Memory: 2060 KB -BaekjoonHub

---
+++ C++17/백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 1e9;
+
+struct Edge
+{
+    int to;
+    int weight;
+};
+
+int n, m;
+int dist[101][101];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == j) dist[i][j] = 0;
+            else dist[i][j] = INF;
+        }
+   }
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        dist[u][v] = min(dist[u][v], w);
+    }
+
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (dist[i][k] != INF && dist[k][j] != INF)
+                {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = 1; j <= n; j++) 
+        {
+            if (dist[i][j] == INF) cout << 0 << " ";
+            else cout << dist[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
